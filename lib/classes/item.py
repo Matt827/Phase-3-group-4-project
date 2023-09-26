87e0d2df6f3fb43de7e5839351b76519743d905b
Matthew <matthew.stewart827@gmail.com>
2023-09-26 09:33:56 -0500

    added documentation, cleaned up code base, formatted code

--- lib/classes/item.py
+++ lib/classes/item.py
@@ -1,7 +1,8 @@
 
 class Item:
-    def __init__(self, name, damage, defense, potion, cost):
+    def __init__(self, name, description, damage, defense, potion, cost):
         self.name = name
+        self.description = description
         self.damage = damage
         self.defense = defense
         self.potion = potion
