ca1f2ec1690aaaccba86252f0198da4b3fb78b9a
Matthew <matthew.stewart827@gmail.com>
2023-09-27 15:52:59 -0500

    added monsters to database

--- lib/classes/monster.py
+++ lib/classes/monster.py
@@ -1,3 +1,4 @@
+from __init__ import CONN, CURSOR
 
 class Monster:
     def __init__(self, name, gold, health, attack, speed, drops):
@@ -8,6 +9,42 @@ class Monster:
         self.attack = attack
         self.speed = speed
         self.drops = drops
+        
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS monsters (
+            MonsterId INTEGER PRIMARY KEY,
+            name TEXT,
+            gold INTEGER,
+            health INTEGER,
+            attack INTEGER,
+            speed INTEGER
+            )
+        """
+        
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS monsters;
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO monsters (name, gold, health, attack, speed)
+            VALUES(?, ?, ?, ?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.gold, self.health, self.attack, self.speed))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+    
         
     def take_damage(self, damage):
         self.hp -= damage
